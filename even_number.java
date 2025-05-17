9cbc07f79f4368bacb25cb2f9467fb3046558a0c
Kulal9922 <105928315+Kulal9922@users.noreply.github.com>
2025-05-17 16:54:34 +0530

    even Number

---
+++ even_number.java
@@ -0,0 +1,11 @@
+class even_number{
+    public static void main(String args[]){
+        int n=10;
+        if(n%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+    }
+}
