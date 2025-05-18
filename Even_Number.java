2df4e699a4109a4a821bdc9cdfda72c976f18241
Kulal9922 <105928315+Kulal9922@users.noreply.github.com>
2025-05-18 14:24:51 +0530

    even number complete

---
+++ Even_Number.java
@@ -0,0 +1,11 @@
+class Even_Number{
+    public static void main(String args[]){
+        int n=10;
+        if(n%2==0){
+            System.out.println("Even number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+    }
+}
